ac445b9afe5d5176bbd839db5ba20effadebe19d
Fernando <fernando-danatas@hotmail.com>
2022-06-10 17:37:39 -0300

    Final

---
+++ src/livros.controller.ts
@@ -0,0 +1,36 @@
+/* eslint-disable prettier/prettier */
+import { LivrosService } from './livros.service';
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Livro } from './livro.model';
+    
+@Controller('livros')
+export class LivrosController {
+    constructor(private livrosService: LivrosService) {
+
+    }
+
+    @Get()
+    async obterTodos(): Promise<Livro[]> {
+        return this.livrosService.obterTodos();
+    }
+
+    @Get(':id')
+    async obterUm(@Param() params): Promise<Livro> {
+        return this.livrosService.obterUm(params.id);
+    }
+
+    @Post()
+    async criar(@Body() livro: Livro) {
+        this.livrosService.criar(livro);
+    }
+
+    @Put()
+    async alterar(@Body() livro: Livro): Promise<[number]> {
+        return this.livrosService.alterar(livro)
+    }
+
+    @Delete(':id')
+    async apagar(@Param() params) {
+        this.livrosService.apagar(params.id);
+    }
+}
